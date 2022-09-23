def1982aef266f25889646786c9aff99b4b30df0
spiritcodefire <negedayinhara@gmail.com>
2022-09-23 13:08:00 +0300

    export component

--- App.js
+++ App.js
@@ -4,35 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DetailsScreen from "./components/screens/DetailsScreen"
 import HomeScreen from "./components/screens/HomeScreen"
+import CreatePostScreen from "./components/screens/CreatePostScreen"
 
 const Stack = createNativeStackNavigator();
-
-function CreatePostScreen({ navigation, route }) {
-  const [postText, setPostText] = React.useState('test');
-
-  return (
-    <>
-      <TextInput
-        multiline
-        placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-        value={postText}
-        onChangeText={setPostText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          // Pass and merge params back to home screen
-          navigation.navigate({
-            name: 'Home',
-            params: { post: postText },
-            merge: true,
-          });
-        }}
-      />
-    </>
-  );
-}
 
 function App() {
   return (
